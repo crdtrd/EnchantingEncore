1e3459d712171f63cd90d6e2f30db10f2618cda7
crdtrd <crdtrd@gmail.com>
2025-08-11 20:39:33 -0700

    Initial commit

---
+++ src/main/java/com/drtdrc/enchantingencore/Enchantingencore.java
@@ -0,0 +1,10 @@
+package com.drtdrc.enchantingencore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Enchantingencore implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
