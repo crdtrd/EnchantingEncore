be78057caecf734175b52f43313ac5af0a3e9487
crdtrd <crdtrd@gmail.com>
2025-08-22 15:32:34 -0700

    Added EntityAttributesMixin

---
+++ src/main/java/com/drtdrc/enchantingencore/mixin/EntityAttributesMixin.java
@@ -0,0 +1,11 @@
+package com.drtdrc.enchantingencore.mixin;
+
+import net.minecraft.entity.attribute.EntityAttributes;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(EntityAttributes.class)
+public class EntityAttributesMixin {
+
+    // either hook into register() or attribute registration
+
+}
