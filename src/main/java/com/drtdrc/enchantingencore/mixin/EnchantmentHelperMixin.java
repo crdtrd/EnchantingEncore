4bd43434914f45ec99fbcf69912da9f5af8ce978
crdtrd <crdtrd@gmail.com>
2025-08-23 15:22:57 -0700

    I accidentally deleted a very important mixin. It is now restored. There seems to be something more going on with the enchantment calculations. A play test shows that the current weighting implementation only kind of works

--- src/main/java/com/drtdrc/enchantingencore/mixin/EnchantmentHelperMixin.java
+++ src/main/java/com/drtdrc/enchantingencore/mixin/EnchantmentHelperMixin.java
@@ -2,15 +2,20 @@
 package com.drtdrc.enchantingencore.mixin;
 
 import com.drtdrc.enchantingencore.BiasContext;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.EnchantableComponent;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 import java.util.function.ToIntFunction;
@@ -18,6 +23,29 @@ import java.util.stream.Stream;
 
 @Mixin(EnchantmentHelper.class)
 public abstract class EnchantmentHelperMixin {
+
+    @Inject(
+            method = "calculateRequiredExperienceLevel",
+            at = @At(value = "HEAD"),
+            cancellable = true
+    )
+    private static void onCalculateExperienceLevel(Random random, int slotIndex, int bookshelfCount, ItemStack stack, CallbackInfoReturnable<Integer> cir) {
+        EnchantableComponent enchantableComponent = stack.get(DataComponentTypes.ENCHANTABLE);
+        if (enchantableComponent == null) {
+            cir.setReturnValue(0);
+        } else {
+            if (bookshelfCount > 50) {
+                bookshelfCount = 50;
+            }
+
+            int i = random.nextInt(4) + 1 + (bookshelfCount >> 1);
+            if (slotIndex == 0) {
+                cir.setReturnValue( Math.max(i / 3, 1) );
+            } else {
+                cir.setReturnValue( slotIndex == 1 ? i * 2 / 3 + 1 : bookshelfCount );
+            }
+        }
+    }
 
     /** Call the real getPossibleEntries (bypasses our redirect). */
     @Invoker("getPossibleEntries")
